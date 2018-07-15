c58f7b6f310ae01491c3dda45d0607df43c08e4e
Mayukh Basu <mayukhbasu5190@gmail.com>
2018-07-16 00:02:37 +0530

    test

--- temp/js/main.js
+++
@@ -1,9 +0,0 @@
-/* jQuery for page scrolling feature */
-
-$(".page-scroll a").click(function() {
-	var $anchor = $(this);
-    $('html, body').animate({
-        scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 600);
-    return false;
-});
